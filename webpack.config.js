f9076b4d0747ddd9a1a111151f1cafbacca9c5de
Yacine Rezgui <rezgui.y@gmail.com>
2015-08-07 14:14:12 +0100

    Add webpack config file

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+var path              = require('path');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  // devtool: 'eval',
+  // debug: true,
+  entry: {
+    main: path.join(__dirname, 'src/main.js')
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].js',
+    publicPath: '/'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract(
+          'style-loader',
+          'css-loader!cssnext-loader'
+        )
+      },
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+        loader: 'url-loader?limit=100000'
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin('[name].css'),
+  ]
+};
