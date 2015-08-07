02ea79f0fd870419c0ca9e5f360787237e9149d3
Yacine Rezgui <rezgui.y@gmail.com>
2015-08-07 18:17:55 +0100

    Display only the most 20 famous repos Create RepositoryItem component

--- src/Projects/index.js
+++ src/Projects/index.js
@@ -1,17 +1,47 @@
 import './Projects.css';
 import React from 'react';
-import Repository from './Repository';
+import RepositoryItem from './RepositoryItem';
+import RepositoryDetails from './RepositoryDetails';
 import repos from '../repos.json';
 
 export default class Projects extends React.Component {
   constructor(props) {
     super(props);
+    this.onSelect = this.onSelect.bind(this);
+    this.state = {
+      selectedRepository: null,
+      repos: this.generateRepositoriesList(repos)
+    };
+  }
+
+  generateRepositoriesList(repos) {
+    let sortedRepos = repos.concat([]);
+
+    sortedRepos.sort(function(repoA, repoB) {
+      return (repoB.stargazers_count + repoB.watchers_count) - (repoA.stargazers_count + repoA.watchers_count);
+    });
+
+    return sortedRepos.slice(0, 20);
+  }
+
+  onSelect(repo) {
+    this.setState({
+      selectedRepository: repo
+    });
   }
 
   render() {
+    if (this.state.selectedRepository) {
+      return (
+        <div className="m3">
+          <RepositoryDetails repo={this.state.selectedRepository} />
+        </div>
+      );
+    }
+
     return (
       <div className="m3 projects">
-      {repos.map((repo, k) => <Repository key={k} name={repo.name} />)}
+      {this.state.repos.map((repo, k) => <RepositoryItem key={k} repo={repo} onSelect={this.onSelect} />)}
       </div>
     );
   }
