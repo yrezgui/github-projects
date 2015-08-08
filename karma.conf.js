3e2e798ec72881384465fa08d7c26a04593644d6
Yacine Rezgui <rezgui.y@gmail.com>
2015-08-08 11:14:11 +0100

    Add Karma & Webpack configuration

---
+++ karma.conf.js
@@ -0,0 +1,51 @@
+var webpack = require('webpack');
+
+module.exports = function (config) {
+  config.set({
+
+    browsers: [ 'Chrome' ],
+
+    singleRun: true,
+
+    frameworks: [ 'mocha' ],
+
+    files: [
+      'tests.webpack.js'
+    ],
+
+    preprocessors: {
+      'tests.webpack.js': [ 'webpack', 'sourcemap' ]
+    },
+
+    reporters: [ 'dots' ],
+
+    webpack: {
+      devtool: 'inline-source-map',
+      module: {
+        loaders: [
+          {
+            test: /\.js$/,
+            loader: 'babel-loader'
+          },
+          {
+            test: /\.css$/,
+            loader: 'style-loader!css-loader!cssnext-loader'
+          },
+          {
+            test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+            loader: 'url-loader?limit=100000'
+          },
+          {
+            test: /\.json$/,
+            loader: 'json-loader'
+          },
+        ],
+      }
+    },
+
+    webpackServer: {
+      noInfo: true
+    }
+
+  });
+};
